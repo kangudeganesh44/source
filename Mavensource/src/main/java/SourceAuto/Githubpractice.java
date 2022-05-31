ed76757861a30bc9fd5ffbe6f30a85510b27e18a
DELL <DELL@DELL>
2022-05-31 14:10:55 +0530

    New Class Added

---
+++ Mavensource/src/main/java/SourceAuto/Githubpractice.java
@@ -0,0 +1,12 @@
+package SourceAuto;
+
+public class Githubpractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Git hub");
+
+	}
+
+}
